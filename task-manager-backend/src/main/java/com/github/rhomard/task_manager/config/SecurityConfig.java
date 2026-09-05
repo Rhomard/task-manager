cead13ed69f1607278fc8f68953e8386474657bd
Rhomard <97520399+Rhomard@users.noreply.github.com>
2026-09-06 00:19:46 +0200

    Allow production frontend origin in CORS config

--- task-manager-backend/src/main/java/com/github/rhomard/task_manager/config/SecurityConfig.java
+++ task-manager-backend/src/main/java/com/github/rhomard/task_manager/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://task-manager-frontend-zi1w.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
